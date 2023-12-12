83dd2ea499f872e4e40232992854b4a45ba82bcc
Alexis Michia <alexi890@outlook.com>
2023-12-12 10:21:44 -0300

    actu

--- page/src/components/Header/Header.js
+++ page/src/components/Header/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">Brandboosters</Link>
+        <Link className="navbar-brand" to="/" style={{ fontWeight: 'bold', textShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)' }}>
+          Brandboosters
+        </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -23,6 +25,19 @@ const Header = () => {
           </ul>
         </div>
       </nav>
+      <style>
+        {`
+          .nav-link {
+            transition: color 0.3s; /* Agregamos una transición para suavizar el cambio de color */
+            font-family: 'cursiva'; /* Cambia 'cursiva' por el nombre de tu tipo de letra cursiva o moderna */
+          }
+
+          .nav-link:hover {
+            color: #ffff00; /* Amarillo fosforescente */
+            border-bottom: 4px solid #ffff00; /* Línea amarilla gruesa en la parte inferior */
+          }
+        `}
+      </style>
     </header>
   );
 }
--- page/src/components/Home/Home.js
+++ page/src/components/Home/Home.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import backgroundImage from '../Home/fondohome.png';  // Ajusta la ruta según tu estructura de carpetas
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Verifica si la ubicación actual es la ruta "/"
+    if (location.pathname === '/') {
+      document.body.style.backgroundImage = `url(${backgroundImage})`;
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundRepeat = 'no-repeat';
+      document.body.style.backgroundPosition = 'center';
+    } else {
+      // Restablece los estilos si la ubicación no es "/"
+      document.body.style.backgroundImage = 'none';
+      document.body.style.backgroundSize = 'auto';
+      document.body.style.backgroundRepeat = 'auto';
+      document.body.style.backgroundPosition = 'auto';
+    }
+
+    // Limpia los estilos cuando el componente se desmonta
+    return () => {
+      document.body.style.backgroundImage = 'none';
+      document.body.style.backgroundSize = 'auto';
+      document.body.style.backgroundRepeat = 'auto';
+      document.body.style.backgroundPosition = 'auto';
+    };
+  }, [location.pathname]);
+
   return (
-    <div className="container mt-5">
+    <div>
       <h1>Bienvenido a Tu Empresa</h1>
       <p>Somos expertos en desarrollo y marketing.</p>
       {/* Agrega más contenido según sea necesario */}
